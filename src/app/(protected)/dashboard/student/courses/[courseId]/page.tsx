39cdf07699488499dac5167f2071065ad1f183e9
AnuragOP01 <ranurag404@gmail.com>
2025-05-31 17:15:32 +0530

    Fixed build errors

--- src/app/(protected)/dashboard/student/courses/[courseId]/page.tsx
+++ src/app/(protected)/dashboard/student/courses/[courseId]/page.tsx
@@ -6,14 +6,14 @@ import CourseAccordion from '@/app/(protected)/(Frontend)/ParticularCourse/Cours
 import { useParams } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   const { courseId } = useParams();
     
   if(!courseId) 
     return <div>Please wait while we fetch the details of your course.</div>
   return (
     <CourseAccordion courseId = {courseId} />
-)
+  )
 }
 
-export default page
+export default Page
